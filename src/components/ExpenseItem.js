2bc33c35fe9136bc7a79f8f0f104928437a72b45
tinithk <tinitse@gmail.com>
2023-10-11 14:05:06 +0530

    PROPS Pass to child to inner child

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItems.css';
 
 function ExpenseItem(props) {
-  const expenseDate = new Date(2023, 10, 11);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 250.25;
+  
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate 
+        date={props.date}
+      />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
