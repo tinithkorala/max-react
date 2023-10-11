ace2f98ce478c9402ebe05d0bfab852c0fad5bdd
tinithk <tinitse@gmail.com>
2023-10-11 13:07:51 +0530

    adding css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItems.css';
+
 function ExpenseItem() {
-    return (<h2>Expense Item</h2>);
+  return (
+    <div className="expense-item">
+      <div>March 21st 2022</div>
+      <div className="expense-item__description">
+        <h2>Car insurance</h2>
+        <div className="expense-item__price">$250.50</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
