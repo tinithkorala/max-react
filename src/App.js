3054a5b94012c0482fa45f4af08b67cf82b20491
tinithk <tinitse@gmail.com>
2023-10-17 19:12:52 +0530

    render statefull lists

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  { id:1, title: 'Car Insuarance', amount: 254.50, date: new Date(2023, 10, 11) },
+  { id:2, title: 'Food', amount: 150.50, date: new Date(2021, 10, 11) },
+  { id:3, title: 'Security', amount: 25.50, date: new Date(2022, 10, 11) },
+];
+
 function App() {
-  const exprenses = [
-    { title: 'Car Insuarance', amount: 254.50, date: new Date(2023, 10, 11) },
-    { title: 'Food', amount: 150.50, date: new Date(2021, 10, 11) },
-    { title: 'Security', amount: 25.50, date: new Date(2022, 10, 11) },
-  ] 
+
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense => {
-    console.log('In app.js');
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   }
 
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses
-        items = {exprenses}
+        items = {expenses}
       />
     </div>
   );
