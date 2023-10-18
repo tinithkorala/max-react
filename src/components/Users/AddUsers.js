9e1ba8d2ce25983a4b47172b3bac6819bcb96a5f
tinithk <tinitse@gmail.com>
2023-10-18 19:57:13 +0530

    button

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from '../UI/Card';
 
 import classes from './AddUser.module.css'
+import Button from '../UI/Button';
 
 const AddUsers = () => {
 
@@ -17,7 +18,9 @@ const AddUsers = () => {
         <input id='username' type='text' />
         <label htmlFor="age">Age</label>
         <input id='age' type='number' />
-        <button type='submit'>Add User</button>
+        <Button
+          type='submit'
+        >Add User</Button>
       </form>
     </Card>
   )
