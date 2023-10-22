bc498396d574ea67f3737fc7061284d2b1920210
tinithk <tinitse@gmail.com>
2023-10-22 17:37:51 +0530

    code fix

--- src/components/Meals/AvilableMeals.js
+++ src/components/Meals/AvilableMeals.js
@@ -33,6 +33,7 @@ const AvilableMeals = () => {
 
   const mealsList = DUMMY_MEALS.map(meal => (
     <MealItem 
+      id={meal.id}
       key={meal.id}
       name={meal.name}
       description={meal.description}
