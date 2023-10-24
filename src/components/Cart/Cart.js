4857e22f096d18990dbcc30c65f1d8f6de252484
tinithk <tinitse@gmail.com>
2023-10-24 18:00:39 +0530

    cart close event

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
 
-const Cart = () => {
+const Cart = (props) => {
 
   const cartItems = <ul className={classes['cart-items']}>{
     [{
@@ -15,14 +15,16 @@ const Cart = () => {
   }</ul>;
 
   return (
-    <Modal>
+    <Modal
+      onClose={props.onClose}
+    >
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
