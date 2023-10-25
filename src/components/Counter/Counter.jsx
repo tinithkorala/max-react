ac1c50a72e49d27fac710d023743be27b2e68b84
tinithk <tinitse@gmail.com>
2023-10-25 13:15:48 +0530

    clever structing

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -27,39 +27,38 @@ function isPrime(number) {
   return true;
 }
 
-const Counter = memo(
-  function Counter({ initialCount }) {
-    log('<Counter /> rendered', 1);
-    const initialCountIsPrime = isPrime(initialCount);
-  
-    const [counter, setCounter] = useState(initialCount);
-  
-    function handleDecrement() {
-      setCounter((prevCounter) => prevCounter - 1);
-    }
-  
-    function handleIncrement() {
-      setCounter((prevCounter) => prevCounter + 1);
-    }
-  
-    return (
-      <section className="counter">
-        <p className="counter-info">
-          The initial counter value was <strong>{initialCount}</strong>. It{' '}
-          <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
-        </p>
-        <p>
-          <IconButton icon={MinusIcon} onClick={handleDecrement}>
-            Decrement
-          </IconButton>
-          <CounterOutput value={counter} />
-          <IconButton icon={PlusIcon} onClick={handleIncrement}>
-            Increment
-          </IconButton>
-        </p>
-      </section>
-    );
+const Counter = function Counter({ initialCount }) {
+  log('<Counter /> rendered', 1);
+  const initialCountIsPrime = isPrime(initialCount);
+
+  const [counter, setCounter] = useState(initialCount);
+
+  function handleDecrement() {
+    setCounter((prevCounter) => prevCounter - 1);
   }
-);
+
+  function handleIncrement() {
+    setCounter((prevCounter) => prevCounter + 1);
+  }
+
+  return (
+    <section className="counter">
+      <p className="counter-info">
+        The initial counter value was <strong>{initialCount}</strong>. It{' '}
+        <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
+      </p>
+      <p>
+        <IconButton icon={MinusIcon} onClick={handleDecrement}>
+          Decrement
+        </IconButton>
+        <CounterOutput value={counter} />
+        <IconButton icon={PlusIcon} onClick={handleIncrement}>
+          Increment
+        </IconButton>
+      </p>
+    </section>
+  );
+}
+
 
 export default Counter;
