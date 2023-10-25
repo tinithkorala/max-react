8c69ba0e8ac73654a86ff17f837c225e4ae79310
tinithk <tinitse@gmail.com>
2023-10-25 12:55:25 +0530

    memo

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, memo } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -27,35 +27,39 @@ function isPrime(number) {
   return true;
 }
 
-export default function Counter({ initialCount }) {
-  log('<Counter /> rendered', 1);
-  const initialCountIsPrime = isPrime(initialCount);
-
-  const [counter, setCounter] = useState(initialCount);
-
-  function handleDecrement() {
-    setCounter((prevCounter) => prevCounter - 1);
+const Counter = memo(
+  function Counter({ initialCount }) {
+    log('<Counter /> rendered', 1);
+    const initialCountIsPrime = isPrime(initialCount);
+  
+    const [counter, setCounter] = useState(initialCount);
+  
+    function handleDecrement() {
+      setCounter((prevCounter) => prevCounter - 1);
+    }
+  
+    function handleIncrement() {
+      setCounter((prevCounter) => prevCounter + 1);
+    }
+  
+    return (
+      <section className="counter">
+        <p className="counter-info">
+          The initial counter value was <strong>{initialCount}</strong>. It{' '}
+          <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
+        </p>
+        <p>
+          <IconButton icon={MinusIcon} onClick={handleDecrement}>
+            Decrement
+          </IconButton>
+          <CounterOutput value={counter} />
+          <IconButton icon={PlusIcon} onClick={handleIncrement}>
+            Increment
+          </IconButton>
+        </p>
+      </section>
+    );
   }
+);
 
-  function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
-  }
-
-  return (
-    <section className="counter">
-      <p className="counter-info">
-        The initial counter value was <strong>{initialCount}</strong>. It{' '}
-        <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
-      </p>
-      <p>
-        <IconButton icon={MinusIcon} onClick={handleDecrement}>
-          Decrement
-        </IconButton>
-        <CounterOutput value={counter} />
-        <IconButton icon={PlusIcon} onClick={handleIncrement}>
-          Increment
-        </IconButton>
-      </p>
-    </section>
-  );
-}
+export default Counter;
