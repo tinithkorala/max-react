696d3f1cb0d9007cdd227c490f5cb7b09c73221c
tinithk <tinitse@gmail.com>
2023-10-26 18:24:03 +0530

    ref forwarding

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import { forwardRef } from 'react'
 
-const ResultModal = ({ result, targetTime }) => {
-  return (
-    <dialog className='result-modal' open>
-      <h2>Your won</h2>
-      <p>The target time was <strong>{targetTime} seconds.</strong></p>
-      <p>You stopped the timer with<strong>X seconds left.</strong></p>
-      <form method='dialog'>
-        <button>Close</button>
-      </form>
-    </dialog>
-  )
-}
+const ResultModal = forwardRef(
+  ({ result, targetTime }, ref) => {
+    return (
+      <dialog ref={ref} className='result-modal'>
+        <h2>Your won</h2>
+        <p>The target time was <strong>{targetTime} seconds.</strong></p>
+        <p>You stopped the timer with<strong>X seconds left.</strong></p>
+        <form method='dialog'>
+          <button>Close</button>
+        </form>
+      </dialog>
+    )
+  }
+)
 
 export default ResultModal
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -4,6 +4,7 @@ import ResultModal from './ResultModal';
 const TimerChallenge = ({ title, targetTime }) => {
 
   const timer = useRef();
+  const dialog = useRef();
 
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
@@ -11,6 +12,7 @@ const TimerChallenge = ({ title, targetTime }) => {
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal();
     }, targetTime * 1000);
     setTimerStarted(true);
   }
@@ -21,7 +23,7 @@ const TimerChallenge = ({ title, targetTime }) => {
 
   return (
     <>
-      {timerExpired && <ResultModal targetTime={targetTime} result="lost" />}
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
       <section className='challenge'>
         <h2>{title}</h2>
         <p className="challenge-time">
