c5761e79791bd503a258c87a13603595bb41ee2d
tinithk <tinitse@gmail.com>
2023-10-29 18:37:12 +0530

    fix

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,12 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+
+  const [remainingTime, setRemainingTime] = useState(TIMER);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log('INTERVAL')
+      setRemainingTime(prevTime => prevTime - 10)
+    }, 10);
+    
+    return () => {
+      clearInterval(interval);
+    }
+  }, [])
 
   useEffect(() => {
     console.log('timer set')
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
 
     return () => {
       console.log('Cleaning up timer');
@@ -26,6 +41,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
     </div>
   );
 }
