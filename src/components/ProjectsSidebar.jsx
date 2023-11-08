38eb77af7c26fe7abd1ac92ee974e948190a73db
tinithk <tinitse@gmail.com>
2023-11-08 19:58:38 +0530

    code

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Button from './Button'
 
 const ProjectsSidebar = () => {
   return (
     <aside className='w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl'>
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
       <div>
-        <button className='w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800'>+ Add Project</button>
+        <Button>
+          + Add Project
+        </Button>
       </div>
       <ul></ul>
     </aside>
