59636b450f5a5ab323d49cf320f0f9e56cc78aea
tinithk <tinitse@gmail.com>
2023-11-12 12:36:25 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAndProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    });
+  }
+
+  let content;
+
+  if(projectState.selectedProjectId === null) {
+    content = <NewProject />
+  }else if(projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected
+      onStartAddProject={handleStartAndProject}
+    />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar 
+        onStartAddProject={handleStartAndProject}
+      />
+      {content}
     </main>
   );
 }
