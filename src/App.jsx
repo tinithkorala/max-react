e2396a4b247e5a42d88840ff0d6777edd14b4a16
tinithk <tinitse@gmail.com>
2023-11-12 13:04:31 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,14 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectState(prevState => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random()
+        id: projectId
       };
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject]
       }
     });
@@ -48,6 +50,7 @@ function App() {
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar 
         onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
       />
       {content}
     </main>
