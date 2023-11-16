63751e99f8bd6142cd989ff50749c7387f91f321
tinithk <tinitse@gmail.com>
2023-11-16 15:25:58 +0530

    fix

--- src/components/Quize.jsx
+++ src/components/Quize.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 
 import QUESTIONS from './../questions.js';
+import quizCompleteImg from './../assets/quiz-complete.png';
 
 const Quize = () => {
 
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers(preUserAnswer => {
@@ -14,13 +16,25 @@ const Quize = () => {
     });
   }
 
+  if(quizIsComplete) {
+    return (
+      <div id='summary'>
+        <img src={quizCompleteImg} alt="Trophy Icon" />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id='quiz'>
       <div id='question'>
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id='answers'>
           {
-            QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            shuffledAnswers.map((answer) => (
               <li key={answer} className='answer'>
                 <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
               </li>
