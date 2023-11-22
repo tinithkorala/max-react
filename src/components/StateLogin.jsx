4a2c27e9082481a88b29cb0528b1a3c3d19ab9c1
tinithk <tinitse@gmail.com>
2023-11-22 08:25:21 +0530

    fix

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,16 +1,24 @@
 import { func } from "prop-types";
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 export default function Login() {
-
-  const email = useRef();
-  const password = useRef();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
-    const enteredEmail = email.current.value;
-    const enteredPassword = password.current.value;
-    console.log(enteredEmail, enteredPassword);
+    console.log(enteredValues);
+  }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
   }
 
   return (
@@ -24,7 +32,8 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
-            ref={email}
+            onChange={(event) => handleInputChange("email", event.target.value)}
+            value={enteredValues.email}
           />
         </div>
 
@@ -34,7 +43,10 @@ export default function Login() {
             id="password"
             type="password"
             name="password"
-            ref={password}
+            onChange={(event) =>
+              handleInputChange("password", event.target.value)
+            }
+            value={enteredValues.password}
           />
         </div>
       </div>
