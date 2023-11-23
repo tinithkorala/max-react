be34a3b9daa7934d88b824a16c8f26906d2029fb
tinithk <tinitse@gmail.com>
2023-11-23 14:33:36 +0530

    fix

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React from "react";
+
+import { currencyFormatter } from "../util/formatting";
 
 const MealItem = ({ meal }) => {
   return (
-    <li className='meal-item'>
+    <li className="meal-item">
       <article>
         <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
-          <p className='meal-item-price'>{meal.price}</p>
-          <p className='meal-item-description'>{meal.description}</p>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
+          <p className="meal-item-description">{meal.description}</p>
         </div>
-        <p className='meal-item-actions'>
+        <p className="meal-item-actions">
           <button>Add to Cart</button>
         </p>
       </article>
     </li>
-  )
-}
+  );
+};
 
-export default MealItem
+export default MealItem;
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -3,13 +3,12 @@ import { useState } from "react";
 import MealItem from "./MealItem";
 
 const Meals = () => {
-
   const [loadedMeals, setLoadedMeals] = useState([]);
 
   useEffect(() => {
     async function fetchMeals() {
-      const response = await fetch('http://localhost:3000/meals');
-      if(!response.ok) {
+      const response = await fetch("http://localhost:3000/meals");
+      if (!response.ok) {
         // ...
       }
       const meals = await response.json();
@@ -20,16 +19,11 @@ const Meals = () => {
 
   return (
     <ul id="meals">
-      {
-        loadedMeals.map(meal => (
-          <MealItem
-            key={meal.id}
-            meal={meal}
-          />
-        ))
-      }
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
     </ul>
-  )
-}
+  );
+};
 
-export default Meals
+export default Meals;
