d8dff3ccb30ea3617b45be8409f00ba1f6a82b01
tinithk <tinitse@gmail.com>
2023-11-23 14:16:07 +0530

    fix

---
+++ src/components/Meals.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Meals = () => {
+
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch('http://localhost:3000/meals');
+      if(!response.ok) {
+        // ...
+      }
+      const meals = await response.json();
+      setLoadedMeals(meals);
+    }
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {
+        loadedMeals.map(meal => (
+          <li key={meal.id}>{meal.name}</li>
+        ))
+      }
+    </ul>
+  )
+}
+
+export default Meals
