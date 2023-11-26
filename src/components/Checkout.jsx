08d4003b6b9b84c591f9da45914cdc69d51a5e55
tinithk <tinitse@gmail.com>
2023-11-26 22:43:31 +0530

    fix

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -5,11 +5,22 @@ import CartContext from '../store/CartContext'
 import Input from './UI/Input'
 import Button from './UI/Button'
 import UserProgressContext from '../store/UserProgressContext'
+import useHttp from '../hooks/useHttp'
+import Error from './Error'
+
+const requestConfig = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
 
 const Checkout = () => {
 
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
+
+  const { data, isLoading: isSending, error, sendRequest } = useHttp('http://localhost:3000/orders', requestConfig);
 
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
@@ -25,18 +36,36 @@ const Checkout = () => {
     const fd = new FormData(event.target);
     const customerData = Object.fromEntries(fd.entries());
 
-    fetch('http://localhost:3000/orders', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        order: {
-          items: cartCtx.items,
-          customer: customerData
-        }
-      })
-    })
+    sendRequest(JSON.stringify({
+      order: {
+        items: cartCtx.items,
+        customer: customerData
+      }
+    }));
+
+    // fetch('http://localhost:3000/orders', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify({
+    //     order: {
+    //       items: cartCtx.items,
+    //       customer: customerData
+    //     }
+    //   })
+    // })
+  }
+
+  let actions = (
+    <>
+      <Button type="button" textOnly>Close</Button>
+      <Button>Submit Order</Button>
+    </>
+  )
+
+  if(isSending) {
+    actions = <span>Sending order data...</span>;
   }
 
   return (
@@ -44,16 +73,18 @@ const Checkout = () => {
       <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-        <Input label="Full Name"type="text"id="name" />
+        <Input label="Full Name" type="text" id="name" />
         <Input label="Email Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
           <Input label="Postal Code" type="text" id="postal-code" />
           <Input label="City" type="text" id="city" />
         </div>
+        {
+          error && <Error title="Failed to submit order" message={error} />
+        }
         <p className='modal-actions'>
-          <Button type="button" textOnly>Close</Button>
-          <Button>Submit Order</Button>
+          {actions}
         </p>
       </form>
     </Modal>
--- src/hooks/useHttp.jsx
+++ src/hooks/useHttp.jsx
@@ -17,10 +17,10 @@ const useHttp = (url, config, initialData) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
-  const sendRequest = useCallback(async function sendRequest() {
+  const sendRequest = useCallback(async function sendRequest(data) {
     setIsLoading(true);
     try {
-      const resData = await sendHttpRequest(url, config);
+      const resData = await sendHttpRequest(url, {...config, body: data});
       setData(resData);
     } catch (error) {
       setError(error.message || 'Something went wrong!');
