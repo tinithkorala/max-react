605efbf8fa4f3524eb4f93cda778e3b89be156c7
tinithk <tinitse@gmail.com>
2023-11-26 15:55:49 +0530

    FIX

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,31 @@ import logoImg from './../assets/logo.jpg';
 import Button from './UI/Button';
 import { useContext } from 'react';
 import CartContext from './../store/CartContext';
+import UserProgressContext from '../store/UserProgressContext';
 
 function Header() {
 
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
   const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
     return totalNumberOfItems + item.quantity;
   }, 0)
 
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
+
   return (
     <header id='main-header'>
       <div id='title'>
-        <img src={logoImg} alt="A resturant" />
+        <img src={logoImg} alt="A restaurant" />
         <h1>ReactFood</h1>
       </div>
       <nav>
         <Button
           textOnly
+          onClick={handleShowCart}
         >
           Cart ({totalCartItems})
         </Button>
---
+++ src/components/UI/Cart.jsx
@@ -0,0 +1,41 @@
+import React, { useContext } from "react";
+import Modal from "./Modal";
+import CartContext, { CartContextProvider } from "../../store/CartContext";
+import { currencyFormatter } from "../../util/formatting";
+import Button from "./Button";
+import UserProgressContext from "../../store/UserProgressContext";
+
+const Cart = () => {
+
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
+
+  return (
+    <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly onClick={handleCloseCart}>Close</Button>
+        <Button>Go to Checkout</Button>
+      </p>
+    </Modal>
+  );
+};
+
+export default Cart;
