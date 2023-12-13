2a45bb85cd4eac2e5793c7bb95e92df54b1fad14
tinithk <tinitse@gmail.com>
2023-12-13 13:43:07 +0530

    fix

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,14 @@
+import {useSelector} from 'react-redux';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+
+  const cartQuantity = useSelector(state => state.totalQuantity);
+
   return (
     <button className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
---
+++ src/store/index.js
@@ -0,0 +1,41 @@
+import {createStore} from 'redux';
+
+const initialState = {
+  cart: [],
+  totalQuantity: 0
+}
+
+const cartReducer = (state = initialState, action) => {
+  if(action.type === 'ADD_ITEM') {
+    const existingCartItemIndex = state.cart.findIndex(item => item.id === action.payload.id);
+    const updatedItems = [...state.cart];
+    if(existingCartItemIndex > -1) {
+      const existingCartItem = state.cart[existingCartItemIndex];
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1
+      }
+      updatedItems[existingCartItemIndex]  = updatedItem;
+    }else {
+      updatedItems.push({...action.payload});
+    }
+    return {
+      ...state,
+      items: updatedItems,
+      totalQuantity: state.totalQuantity + 1
+    }
+    // console.log(action.payload);
+    // return {
+    //   ...state,
+    //   items: action.payload
+    // }
+  }
+  // if(action.type === 'REMOVE_ITEM') {
+
+  // }
+  return state;
+}
+
+const store = createStore(cartReducer);
+
+export default store;
