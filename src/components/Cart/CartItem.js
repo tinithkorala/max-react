3424c5e0e7d37d4e21473e648cc5f1c8b5bca21e
tinithk <tinitse@gmail.com>
2023-12-13 14:11:16 +0530

    fix

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,19 @@
+import { useDispatch } from 'react-redux';
 import classes from './CartItem.module.css';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+
+  const {id, title, quantity, total, price } = props.item;
+
+  const dispatch = useDispatch();
+
+  const removeItemHandler = () => {
+    dispatch({type: "REMOVE_ITEM", payload: id})
+  }
+
+  const addItemHandler = () => {
+    dispatch({type: "ADD_ITEM", payload: {id, title, price}})
+  }
 
   return (
     <li className={classes.item}>
@@ -17,8 +29,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
         </div>
       </div>
     </li>
