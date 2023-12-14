12b6bb3d6673c7f7e1110f0210ebd705fb8d4dea
tinithk <tinitse@gmail.com>
2023-12-14 12:29:16 +0530

    fix

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Home() {
+
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate('/products');
+  }
+
   return (
     <>
       <h1>Home</h1>
       <p>
         Go to <Link to="/products">the list of products</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
       </p>
     </>
   );
