328802f2dfc473b93e8b6cf775cf7583e8896ea0
tinithk <tinitse@gmail.com>
2023-12-14 07:57:30 +0530

    old router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createRoutesFromElements, createBrowserRouter, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 
-const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/products", element: <Products /> },
-]);
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<Home />} />  
+    <Route path="/products" element={<Products />} />  
+  </Route>
+)
+
+// const router = createBrowserRouter([
+//   { path: "/", element: <Home /> },
+//   { path: "/products", element: <Products /> },
+// ]);
+
+const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
