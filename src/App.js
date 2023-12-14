86c9718b315623c8fdf2d96cc402e5f1a66e40de
tinithk <tinitse@gmail.com>
2023-12-14 07:59:10 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import { RouterProvider, createRoutesFromElements, createBrowserRouter, Route } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<Home />} />  
-    <Route path="/products" element={<Products />} />  
-  </Route>
-)
-
-// const router = createBrowserRouter([
-//   { path: "/", element: <Home /> },
-//   { path: "/products", element: <Products /> },
-// ]);
-
-const router = createBrowserRouter(routeDefinitions);
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Products /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
