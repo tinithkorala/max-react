ac0dc11cc40b64af2032e35a0cbe8651a7c0fa1f
tinithk <tinitse@gmail.com>
2023-12-14 08:04:37 +0530

    Link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <h1>Home</h1>
-  )
+    <>
+      <h1>Home</h1>
+      <p>
+        Go to <Link to="/products">the list of products</Link>
+      </p>
+    </>
+  );
 }
 
-export default Home
+export default Home;
