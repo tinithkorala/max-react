a7edcb0b18cb47d23e38a9cb244835ef08b5586a
tinithk <tinitse@gmail.com>
2023-12-14 16:28:57 +0530

    code

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
 
 const EventDetail = () => {
-  return (
-    <h1>EventDetail</h1>
-  )
-}
+  const params = useParams();
 
-export default EventDetail
+  return (
+    <>
+      <h1>EventDetail</h1>
+      <p>Event Id: {params.eventId}</p>
+    </>
+  );
+};
+
+export default EventDetail;
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Some event",
+  },
+  {
+    id: "e2",
+    title: "Another event",
+  },
+];
 
 const Events = () => {
   return (
-    <h1>Events</h1>
-  )
-}
+    <>
+      <h1>EventsPage</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
-export default Events
+export default Events;
