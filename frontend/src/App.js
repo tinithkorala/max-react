fc638daf970ea05604c2fe100fd6b3a9f6609ff3
tinithk <tinitse@gmail.com>
2023-12-14 17:05:55 +0530

    loaders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,19 @@ const router = createBrowserRouter([
         path: "events",
         element: <EventsRootLayout />,
         children: [
-          { index: true, element: <Events /> },
+          {
+            index: true,
+            element: <Events />,
+            loader: async () => {
+              const response = await fetch("http://localhost:8080/events");
+              if (!response.ok) {
+                //...
+              } else {
+                const resData = await response.json();
+                return resData.events;
+              }
+            },
+          },
           { path: ":eventId", element: <EventDetail /> },
           { path: "new", element: <NewEvent /> },
           { path: ":eventId/edit", element: <EditEvent /> },
