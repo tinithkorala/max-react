204b3280764a22ddb0953d29c554d4db84d681c7
tinithk <tinitse@gmail.com>
2023-12-14 08:53:48 +0530

    error page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <Home /> },
       { path: "/products", element: <Products /> },
