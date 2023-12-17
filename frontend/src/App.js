84a3786e73b850f01d9ebdc8ba03fe2d74da1cfd
tinithk <tinitse@gmail.com>
2023-12-17 11:53:52 +0530

    loader function more usage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
-import Events from "./pages/Events";
+import Events, { loader as eventsLoader } from "./pages/Events";
 import EventDetail from "./pages/EventDetail";
 import NewEvent from "./pages/NewEvent";
 import EditEvent from "./pages/EditEvent";
@@ -20,15 +20,7 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <Events />,
-            loader: async () => {
-              const response = await fetch("http://localhost:8080/events");
-              if (!response.ok) {
-                //...
-              } else {
-                const resData = await response.json();
-                return resData.events;
-              }
-            },
+            loader: eventsLoader
           },
           { path: ":eventId", element: <EventDetail /> },
           { path: "new", element: <NewEvent /> },
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -11,3 +11,13 @@ function Events() {
 }
 
 export default Events;
+
+export const loader = async () => {
+  const response = await fetch("http://localhost:8080/events");
+  if (!response.ok) {
+    //...
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
